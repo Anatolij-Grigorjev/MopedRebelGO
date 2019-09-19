5c8648c112e3a7fec5533f65e1fd45c066fce3bf
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-09-19 20:05:58 +0300

    Add aspects of moped packages bundle

---
+++ game/rebel/packages_bundle.gd
@@ -0,0 +1,15 @@
+extends Node2D
+class_name PackagesBundle
+"""
+Controller for a bundle of packages
+Keep track of current packages count, fires off a package 
+every time the moped hits an obstacle
+
+When the last package has been thrown off and and the moped hit an obstacle
+this bundle signals need for gameover
+"""
+
+
+
+func _ready() -> void:
+	pass # Replace with function body.
