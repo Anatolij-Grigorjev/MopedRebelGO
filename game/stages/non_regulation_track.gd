060456043b4aa0ba1d773ebcd514ad0563e72f45
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-09-23 18:50:04 +0300

    start work NRT tile light

---
+++ game/stages/non_regulation_track.gd
@@ -0,0 +1,13 @@
+extends Node2D
+class_name NonRegulationTrack
+"""
+Controller for non regulation track segment. 
+This records when moped enters the track, when it leaves the track and the actual 
+in-track distance travelled. 
+The light under this track segment changes color 
+based on moped presence on that part of track
+"""
+
+
+func _ready():
+	pass # Replace with function body.
