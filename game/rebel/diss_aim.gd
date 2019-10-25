e524658455478098d53891514c6a8dfbc943d3fe
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-10-25 19:38:57 +0300

    Initial nodes for aiming diss at civilian for custom drawing

---
+++ game/rebel/diss_aim.gd
@@ -0,0 +1,10 @@
+extends Node2D
+"""
+Visual indicator for aiming at a specific civilian. Texture is custom drawn,
+runs along the ground from civilian to moped
+"""
+
+
+
+func _ready():
+	pass # Replace with function body.
