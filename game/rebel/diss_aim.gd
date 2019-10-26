442d8af173bb4275f7334a1921797fb35e052c34
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-10-26 14:27:58 +0300

    WIP rebel to citizen channel, simplify draw

--- game/rebel/diss_aim.gd
+++ game/rebel/diss_aim.gd
@@ -5,58 +5,44 @@ Visual indicator for aiming at a specific civilian. Texture is custom drawn,
 runs along the ground from civilian to moped
 """
 
-export(Color) var channel_modulate: Color = Color.white
+export(Color) var channel_modulate: Color = Color(0.5, 0.5, 0.5, 0.5)
 export(float) var channel_width: float = 100
 
 
 var _channel_body: Texture = preload("res://stages/tile_light_texture.png")
-var _channel_start_position: Vector2
-var _channel_end_position: Vector2
+var _channel_start_node: Node2D
+var _channel_end_node: Node2D
 var _channel_present: bool = false
 
 
 func _ready() -> void:
-	_channel_present = true
-	_channel_start_position = Vector2(100, 600)
-	_channel_end_position = Vector2(500, 450)
+
 	pass # Replace with function body.
 	
 	
 func _process(delta: float) -> void:
-	if (Input.is_action_just_released("debug1")):
-		_channel_end_position.x -= 25
-	if (Input.is_action_just_released("debug2")):
-		_channel_end_position.x += 25
-	if (Input.is_action_just_released("swerve_up")):
-		_channel_end_position.y -= 25
-	if (Input.is_action_just_released("swerve_down")):
-		_channel_end_position.y += 25
-	update()
-	
+	if (_channel_present):
+		update()
+		
 	
 func _draw() -> void:
 	if (not _channel_present):
 		return
 	
-	var start := _channel_start_position
-	var end := _channel_end_position
+	var start := _channel_start_node.global_position
+	var end := _channel_end_node.global_position
 	
 	var end_distance: float = end.x - start.x
-	if (end_distance <= 0):
+	if (end_distance <= channel_width):
+		_stop_channel()
 		return
-	
-	#find channel angle
-	var angle_vector := Vector2(end_distance, abs(end.y - start.y))
-	var channel_angle_radians : float = angle_vector.angle()
-	var angle_sin := sin(channel_angle_radians)
-	var angle_cos := cos(channel_angle_radians)
 	
 	#create channel points
 	var extents := channel_width / 2
-	var channel_top_left := Vector2(start.x * angle_cos, (start.y - extents) * angle_sin)
-	var channel_top_right := Vector2(end.x * angle_cos, (end.y - extents) * angle_sin)
-	var channel_bottom_right := Vector2(end.x * angle_cos, (end.y + extents) * angle_sin)
-	var channel_bottom_left := Vector2(start.x * angle_cos, (start.y + extents) * angle_sin)
+	var channel_top_left := Vector2(start.x, start.y - extents)
+	var channel_top_right := Vector2(end.x, end.y - extents)
+	var channel_bottom_right := Vector2(end.x, end.y + extents)
+	var channel_bottom_left := Vector2(start.x, start.y + extents)
 	
 	#draw poly
 	var points_array : PoolVector2Array = [
@@ -65,3 +51,27 @@ func _draw() -> void:
 	]
 #	draw_polyline(points_array, channel_modulate, channel_width)
 	draw_colored_polygon(points_array, channel_modulate, [], _channel_body)
+
+
+func _stop_channel() -> void:
+	_channel_present = false
+	_channel_start_node = null
+	_channel_end_node = null
+	#perform last redraw to clear screen
+	update()
+	
+	
+"""
+Set start and end nodes to track for channel
+This initialized channel with current positions of the nodes and a desire 
+to draw itself. Once the positions converge and the channel can no longer be drawn 
+it will disable itself
+"""
+func start_channel_between(start: Node2D, end: Node2D) -> void:
+	_channel_start_node = start
+	_channel_end_node = end
+	_channel_present = true
+	
+	
+func is_channel_active() -> bool:
+	return _channel_present
