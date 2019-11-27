6a141a1b7bd8f626d48d72757511a7c60969c288
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-11-27 12:17:53 +0200

    WIP stage select screen, fix stage cell tool

--- game/gui/select_level_cell.gd
+++ game/gui/select_level_cell.gd
@@ -11,7 +11,7 @@ export(bool) var selected: bool = false setget set_selected
 
 
 func get_level_name() -> String:
-	var level_name_label: Label = $VBoxContainer/LabelMarginContainer/Label
+	var level_name_label: Label = $SelectedBorder/VBoxContainer/LabelMarginContainer/Label
 	if (level_name_label):
 		return level_name_label.text
 	else:
@@ -19,18 +19,21 @@ func get_level_name() -> String:
 		
 		
 func set_level_name(name: String) -> void:
-	var level_name_label: Label = $VBoxContainer/LabelMarginContainer/Label
+	var level_name_label: Label = $SelectedBorder/VBoxContainer/LabelMarginContainer/Label
 	if (level_name_label):
 		level_name_label.text = name
 
 
-func set_selected(selected: bool) -> void:
-	var level_name_label: Label = $VBoxContainer/LabelMarginContainer/Label
-	var thumbnail_cover: Control = $VBoxContainer/ThumbnailMarginContainer/Cover
-	self.selected = selected
+func set_selected(is_selected: bool) -> void:
+	var level_name_label: Label = $SelectedBorder/VBoxContainer/LabelMarginContainer/Label
+	var thumbnail_cover: Control = $SelectedBorder/VBoxContainer/ThumbnailMarginContainer/Cover
+	var select_border: Control = $SelectedBorder
+	selected = is_selected
 	if (selected):
 		level_name_label.modulate = Color.red
 		thumbnail_cover.visible = true
+		select_border.self_modulate.a = 255
 	else:
 		level_name_label.modulate = Color.white
 		thumbnail_cover.visible = false
+		select_border.self_modulate.a = 0
