80946338f231c211f5360dfd44381619adb34e53
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-12-18 19:08:31 +0200

    WIP GUI component for timed bonus

---
+++ game/gui/timed_bonus.gd
@@ -0,0 +1,20 @@
+extends TextureProgress
+class_name TimedBonus
+"""
+Gui element that has a default value which resets after a timeout. 
+Time to reset indicated by diminishing progress bar
+"""
+
+#TODO: export
+#timeout time
+#default value
+#do setget
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
