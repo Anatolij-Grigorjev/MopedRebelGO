c2f758f46b30fd1ad6609bff85595f7d24dffea8
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2019-12-22 09:48:08 +0200

    WIP stage progress tracker bar

--- game/gui/stage_progress.gd
+++ game/gui/stage_progress.gd
@@ -1,4 +1,4 @@
-extends ProgressBar
+extends TextureProgress
 class_name StageProgressBar
 """
 Progress bar that shows current progress of node in stage.
@@ -6,11 +6,7 @@ Node to track is assigned via path, stage bounds interval as well
 """
 
 
-export(NodePath) var track_node_path setget set_track_path
-export(Vector2) var stage_bounds setget set_bounds
-
-
-onready var track_node: Node2D = get_node(track_node_path)
+export(Vector2) var stage_bounds setget _set_bounds
 
 
 func _ready() -> void:
@@ -18,16 +14,10 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if (is_instance_valid(track_node)):
-		self.value = track_node.global_position.x
+	pass
 	
 
-func set_bounds(bounds: Vector2) -> void:
+func _set_bounds(bounds: Vector2) -> void:
 	stage_bounds = bounds
-	self.min_value = bounds.x
-	self.max_value = bounds.y 
-	
-	
-func set_track_path(path: NodePath) -> void:
-	track_node_path = path
-	track_node = get_node(path)
+	min_value = bounds.x
+	max_value = bounds.y 
